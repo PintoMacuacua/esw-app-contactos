7ac5f10627694aeadd90c5d32c12289f27c20faa
Pinto Macuacua <pintomacuacua3@gmail.com>
2025-06-07 15:28:21 +0200

    feat (REPOSITORY, Service CLASS) : Utilizador e Contacto

--- app-contactos/src/main/java/com/esw/app_contactos/service/ContactoService.java
+++ app-contactos/src/main/java/com/esw/app_contactos/service/ContactoService.java
@@ -1,5 +1,31 @@
 package com.esw.app_contactos.service;
 
-public class ContactoService {
+import org.springframework.stereotype.Service;
 
+import com.esw.app_contactos.dto.ContactoDTO;
+import com.esw.app_contactos.model.Contacto;
+import com.esw.app_contactos.model.Utilizador;
+import com.esw.app_contactos.repository.ContactoRepository;
+import com.esw.app_contactos.repository.UtilizadorRepository;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class ContactoService {
+    private final ContactoRepository contactoRepo;
+    private final UtilizadorRepository utilizadorRepo;
+
+    public ContactoDTO createContacto(ContactoDTO dto) {
+        Utilizador utilizador = utilizadorRepo.findById(dto.utilizadorId())
+            .orElseThrow();
+
+        Contacto contacto = new Contacto();
+        contacto.setNome(dto.nome());
+        contacto.setTelefone(dto.telefone());
+        contacto.setUtilizador(utilizador);
+        contacto = contactoRepo.save(contacto);
+
+        return new ContactoDTO(contacto.getId(), contacto.getNome(), contacto.getTelefone(), utilizador.getId());
+    }
 }
--- app-contactos/src/main/java/com/esw/app_contactos/service/UtilizadorService.java
+++ app-contactos/src/main/java/com/esw/app_contactos/service/UtilizadorService.java
@@ -1,5 +1,23 @@
 package com.esw.app_contactos.service;
 
-public class UtilizadorService {
+import org.springframework.stereotype.Service;
 
+import com.esw.app_contactos.dto.UtilizadorDTO;
+import com.esw.app_contactos.model.Utilizador;
+import com.esw.app_contactos.repository.UtilizadorRepository;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class UtilizadorService {
+    private final UtilizadorRepository utilizadorRepo;
+
+    public UtilizadorDTO createUtilizador(UtilizadorDTO dto) {
+        Utilizador utilizador = new Utilizador();
+        utilizador.setNome(dto.nome());
+        utilizador.setEmail(dto.email());
+        utilizador = utilizadorRepo.save(utilizador);
+        return new UtilizadorDTO(utilizador.getId(), utilizador.getNome(), utilizador.getEmail());
+    }
 }
