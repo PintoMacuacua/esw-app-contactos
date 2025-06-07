5a01fd785222edf6b3d3dccaa03bae0f9cfca88e
Pacotê <89321178+PintoMacuacua@users.noreply.github.com>
2025-06-07 14:42:34 +0200

    feat(h2-database) : base de dados h2
    Adicionada a Configuracao da Base de dados local H2

---
+++ app-contactos/src/main/java/com/esw/app_contactos/AppContactosApplication.java
@@ -0,0 +1,13 @@
+package com.esw.app_contactos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppContactosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppContactosApplication.class, args);
+	}
+
+}
